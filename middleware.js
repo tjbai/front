935c2b1c914bdf9269e8feae5be253f96d8a6c83
TJ Bai <tjianbai@gmail.com>
2025-01-20 10:16:18 -0500

    update research description

--- middleware.js
+++ middleware.js
@@ -2,10 +2,5 @@ import { next, rewrite } from "@vercel/edge";
 
 export default function middleware(req) {
   const url = new URL(req.url);
-
-  if (url.pathname != "/") {
-    return rewrite(new URL(url.pathname, "https://blog.tjbai.me"));
-  }
-
   return next();
 }
